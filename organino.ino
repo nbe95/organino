508c7cfb08fbad6f8fc8cb37279d424681bb2331
Niklas Bettgen <niklas@bettgen.de>
2023-04-26 13:32:14 +0200

    Add rotary pedal

--- organino.ino
+++ organino.ino
@@ -1,31 +1,42 @@
 #include <MIDI.h>
 #include <midi_Defs.h>
 
-enum State : byte {
-    Off,
-    On,
-    Holding
-};
+#include "timer.h"
 
+
+// Global note and sustain status
+enum State : byte { Off, On, Holding };
 State NOTES[127] = { State::Off };
 bool SUSTAIN_ACTIVE = false;
+
+// Timer for rotary pedal pulse
+Timer ROTARY_TIMER(50);
 
 // Create and bind the MIDI interface to the default hardware Serial port
 MIDI_CREATE_DEFAULT_INSTANCE();
 
 void setup() {
+    // LED indication
     digitalWrite(LED_BUILTIN, LOW);
 
+    // Set up MIDI interface on Serial port
     MIDI.begin(MIDI_CHANNEL_OMNI);
     MIDI.setThruFilterMode(midi::Thru::Mode::Off);
 
+    // Register callbacks
     MIDI.setHandleNoteOn(onNoteOn);
     MIDI.setHandleNoteOff(onNoteOff);
     MIDI.setHandleControlChange(onControlChange);
 }
 
 void loop() {
+    // Handle incoming MIDI traffic
     MIDI.read();
+
+    // Reset pedal pulse for the rotary speed
+    if (ROTARY_TIMER.check()) {
+        digitalWrite(PIN_ROTARY_SPEED, LOW);
+    }
 }
 
 void onNoteOn(const byte channel, const byte note, const byte velocity) {
@@ -49,10 +60,20 @@ void onNoteOff(const byte channel, const byte note, const byte velocity) {
 }
 
 void onControlChange(const byte channel, const byte number, const byte value) {
-    // Note: Since the Sustain pedal cannot be divided to different  parts on
-    // our Clavinova, we're simply using Sustenuto as a workaround...
-    if (number == midi::MidiControlChangeNumber::Sostenuto) {
+    // Since the Sustain pedal cannot be divided to different parts on
+    // our Clavinova, we're simply using Sustenuto as a workaround.
+    if (number == midi::MidiControlChangeNumber::Sostenuto)
         onSustainPedal(channel, value >= 64);
+
+    // Any positive edge of an incoming Portamento will trigger rotary speed.
+    if (number == midi::MidiControlChangeNumber::Portamento)
+        onPortamentoPedal(value >= 64);
+}
+
+void onPortamentoPedal(const bool active) {
+    if (active) {
+        digitalWrite(PIN_ROTARY_SPEED, HIGH);
+        ROTARY_TIMER.restart();
     }
 }
 
