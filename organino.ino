0e8872bf432f8c39d2583d452ce953f7c3a79ca6
Niklas Bettgen <niklas@bettgen.de>
2023-04-26 12:01:41 +0200

    Use 3 states for each note to prevent double notes and cut-offs

--- organino.ino
+++ organino.ino
@@ -1,15 +1,14 @@
 #include <MIDI.h>
 #include <midi_Defs.h>
 
-#define BIT_ARRAY_TYPE unsigned int
-#define BIT_ARRAY_SIZE (sizeof(BIT_ARRAY_TYPE) * 8)
-#define BIT_SET(A, k) (A[(k) / BIT_ARRAY_SIZE] |= (1 << ((k) % BIT_ARRAY_SIZE)))
-#define BIT_GET(A, k) (A[(k) / BIT_ARRAY_SIZE] & (1 << ((k) % BIT_ARRAY_SIZE)))
-#define BIT_CLEAR(A, k) (A[(k) / BIT_ARRAY_SIZE] &= ~(1 << ((k) % BIT_ARRAY_SIZE)))
+enum State : byte {
+    Off,
+    On,
+    Holding
+};
 
-// Global bit array for note states (8*16 = 128 bits)
-BIT_ARRAY_TYPE NOTES[8] = {0};
-bool SUSTAIN_STATE = false;
+State NOTES[127] = { State::Off };
+bool SUSTAIN_ACTIVE = false;
 
 // Create and bind the MIDI interface to the default hardware Serial port
 MIDI_CREATE_DEFAULT_INSTANCE();
@@ -30,14 +29,20 @@ void loop() {
 }
 
 void onNoteOn(const byte channel, const byte note, const byte velocity) {
-    MIDI.sendNoteOn(note, velocity, channel);
-    BIT_SET(NOTES, note & 0x7f);
+    if (NOTES[note & 0x7f] == State::Off) {
+        MIDI.sendNoteOn(note, velocity, channel);
+        NOTES[note & 0x7f] = State::On;
+    }
 }
 
 void onNoteOff(const byte channel, const byte note, const byte velocity) {
-    if (!SUSTAIN_STATE) {
-        MIDI.sendNoteOff(note, 0, channel);
-        BIT_CLEAR(NOTES, note & 0x7f);
+    if (NOTES[note & 0x7f] == State::On) {
+        if (SUSTAIN_ACTIVE) {
+            NOTES[note & 0x7f] = State::Holding;
+        } else {
+            MIDI.sendNoteOff(note, 0, channel);
+            NOTES[note & 0x7f] = State::Off;
+        }
     }
 }
 
@@ -48,14 +53,14 @@ void onControlChange(const byte channel, const byte number, const byte value) {
 }
 
 void onSustainPedal(const byte channel, const bool active) {
-    SUSTAIN_STATE = active;
+    SUSTAIN_ACTIVE = active;
     digitalWrite(LED_BUILTIN, active ? HIGH : LOW);
 
     if (!active) {
         for (byte note = 0; note < 127; note++) {
-            if (BIT_GET(NOTES, note)) {
+            if (NOTES[note & 0x7f] == State::Holding) {
                 MIDI.sendNoteOff(note, 0, channel);
-                BIT_CLEAR(NOTES, note);
+                NOTES[note & 0x7f] = State::Off;
             }
         }
     }
